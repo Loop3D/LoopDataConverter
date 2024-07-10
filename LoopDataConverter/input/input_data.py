4f73437a33ca62304206ac0ce3127da3219a4c01
rabii-chaarani <rabii.chaarani@monash.edu>
2024-07-10 13:34:12 +0930

    docs: add docstrings to the input module

--- LoopDataConverter/input/input_data.py
+++ LoopDataConverter/input/input_data.py
@@ -5,6 +5,15 @@ from ..datatypes import Datatype
 
 @dataclass
 class InputData:
+    """ Class to store input data for the loop data converter
+    
+    Attributes:
+    geology: Datatype.GEOLOGY = None 
+    structure: Datatype.STRUCTURE = None
+    fault: Datatype.FAULT = None
+    fold: Datatype.FOLD = None
+    
+    """
     geology: Datatype.GEOLOGY = None
     structure: Datatype.STRUCTURE = None
     fault: Datatype.FAULT = None
@@ -12,6 +21,14 @@ class InputData:
 
 
     def __getitem__(self, datatype: Datatype):
+        """ Method to get the the file directory of a datatype
+
+        Parameters:
+            datatype (Datatype): The datatype to get the file directory of
+
+        Returns:
+            The file directory of the datatype
+        """
 
         return self.__dict__[datatype]
 
