a343c734c32f9bd53051a4d7119a45cad4732c6d
rabii-chaarani <rabii.chaarani@monash.edu>
2024-07-31 13:33:06 +0930

    fix: added correct imports

--- LoopDataConverter/converters/__init__.py
+++ LoopDataConverter/converters/__init__.py
@@ -1,14 +1,4 @@
 from .base_converter import BaseConverter
 from .ntgs_converter import NTGSConverter
+from .loop_converter import LoopConverter
 from ..datatypes import SurveyName
-
-converter_map = {
-    SurveyName.NTGS: NTGSConverter,
-    SurveyName.GA: "",
-    SurveyName.GSQ: "",
-    SurveyName.GSWA: "",
-    SurveyName.GSSA: "",
-    SurveyName.GSV: "",
-    SurveyName.MRT: "",
-    SurveyName.GSNSW: "",
-}
